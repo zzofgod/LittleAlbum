55fbbb0165941db94d922cefeaa2a210b7cfed48
15897710445 <l>
2019-05-23 14:37:33 +0800

    upload

---
+++ app.js
@@ -0,0 +1,35 @@
+﻿var express = require("express");
+var bodyParser = require('body-parser')
+var app = express();
+//控制器
+var router = require("./controller");
+
+//设置模板引擎
+app.set("view engine", "ejs");
+
+//路由中间件，静态页面
+app.use(express.static("./public"));
+app.use(express.static("./uploads"));
+
+// 解析 application/json
+app.use(bodyParser.json());
+// 解析 application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//首页
+app.get("/", router.showIndex);
+app.get("/:albumName", router.showAlbum);
+app.get("/up", router.showUp);
+app.post("/up", router.doPost);
+app.get("/createdir", router.createDir);
+app.post("/createdir", router.doPostdir);
+
+//404
+app.use(function (req, res) {
+    res.render("err");
+});
+
+app.listen(3100,function(err){
+  if(err) throw err
+ console.log("running in 3100")
+});
---
+++ models/file.js
@@ -0,0 +1,76 @@
+var fs = require("fs");
+var wenjianjia = []
+//这个函数的callback中含有两个参数，一个是err
+//另一个是存放所有文件夹名字的array。
+exports.getAllAlbums = function (callback) {
+    fs.readdir("./uploads", function (err, files) {
+        if (err) {
+            callback("没有找到uploads文件", null);
+        }
+        var allAlbums = [];
+        (function iterator(i) {
+            if (i == files.length) {
+                //遍历结束
+                callback(null, allAlbums);
+                return;
+            }
+            fs.stat("./uploads/" + files[i], function (err, stats) {
+                if (err) {
+                    callback("找不到文件" + files[i], null);
+                }
+                if (stats.isDirectory()) {
+                    allAlbums.push(files[i]);
+                }
+                iterator(i + 1);
+            });
+
+        })(0);
+    });
+}
+
+//通过文件名，得到所有图片
+exports.getAllImagesByAlbumName = function (albumName, callback) {
+    fs.readdir("./uploads/" + albumName, function (err, files) {
+        if (err) {
+            callback("没有找到uploads文件", null);
+            return;
+        }
+        var allImages = [];
+        (function iterator(i) {
+            if (i == files.length) {
+                //遍历结束
+                console.log(allImages);
+                callback(null, allImages);
+                return;
+            }
+            fs.stat("./uploads/" + albumName + "/" + files[i], function (err, stats) {
+                if (err) {
+                    callback("找不到文件" + files[i], null);
+                    return;
+                }
+                if (stats.isFile()) {
+                    allImages.push(files[i]);
+                }
+                iterator(i + 1);
+            });
+        })(0);
+    });
+}
+
+exports.createdir = (data, callback) => {
+    fs.readdir("./uploads", (err, files) => {
+        if (err) callback(err)
+        console.log(files)
+        const dir = files.find((value) => {
+            return value == data.dirname
+        })
+        if (dir === undefined) {
+            fs.mkdir("./uploads/" + data.dirname, (err) => {
+                if (err) callback(err)
+                callback(null)
+            })
+        } else {
+            callback(null)
+        }
+    })
+}
